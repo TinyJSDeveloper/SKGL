278639204defe938e1a25472ebf5c2d29f0202a3
TinyJSDeveloper <TinyJSDeveloper@users.noreply.github.com>
2018-12-07 19:51:20 -0300

    Update TileSprite.lua

--- lib/skgl/sprite/TileSprite.lua
+++ lib/skgl/sprite/TileSprite.lua
@@ -47,6 +47,7 @@ function TileSprite.new(width, height)
   function def:setMapSize(mapWidth, mapHeight)
     self.mapWidth = mapWidth or self.mapWidth
     self.mapHeight = mapHeight or self.mapHeight
+    self:resizeBounds()
   end
 
   --[[
@@ -91,12 +92,36 @@ function TileSprite.new(width, height)
   --]]
   function  def:drawFrameColumns()
     -- Contadores de tamanho (largura e altura):
-    local width = 0
+    local width =  0
     local height = 0
 
     -- Salvar tamanho original dos tiles (eles serão restaurados depois):
     local tileWidth = self.width
     local tileHeight = self.height
+
+    -- Salvar tamanho original do mapa (ele será restaurado depois):
+    local mapWidth = self.mapWidth
+    local mapHeight = self.mapHeight
+
+    -- Adiantar contador de largura para não iterar sobre tiles desnecessários:
+    if self.offsetX < -self.width then
+      width = math.floor(math.abs(self.offsetX / self.width)) * self.width
+    end
+
+    -- Adiantar contador de altura para não iterar sobre tiles desnecessários:
+    if self.offsetY < -self.height then
+      height = math.floor(math.abs(self.offsetY / self.height)) * self.height
+    end
+
+    -- Alterar a largura do mapa para não iterar sobre tiles desnecessários:
+    if (self.offsetX + self.mapWidth) > Display.getWidth() then
+      self.mapWidth = Display.getWidth() - self.offsetX
+    end
+
+    -- Alterar a altura do mapa para não iterar sobre tiles desnecessários:
+    if (self.offsetY + self.mapHeight) > Display.getHeight() then
+      self.mapHeight = Display.getHeight() - self.offsetY
+    end
 
     while height < self.mapHeight do
       -- Desenhar tiles de forma ordenada...
@@ -115,6 +140,18 @@ function TileSprite.new(width, height)
     -- Restaurar tamanho original dos tiles:
     self.width = tileWidth
     self.height = tileHeight
+
+    -- Restaurar tamanho original do mapa:
+    self.mapWidth = mapWidth
+    self.mapHeight = mapHeight
+  end
+
+  --[[
+  - @override
+  - @return {boolean} Determina se o sprite está dentro da tela.
+  --]]
+  function def:isInsideScreen()
+    return Surface.isInsideScreen(self.offsetX, self.offsetY, self.mapWidth, self.mapHeight)
   end
 
   --[[
@@ -136,7 +173,7 @@ function TileSprite.new(width, height)
     self:applySpeed()
 
     -- Desenhar o sprite:
-    if self.visible == true and self.opacity > 0.0 then
+    if self.visible == true and self.opacity > 0.0 and self:isInsideScreen() == true then
 
       -- Desenhar um retângulo colorido (cor de fundo):
       self:drawColor(self.color, self.offsetX, self.offsetY, self.mapWidth, self.mapHeight)
